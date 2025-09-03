535cb6555945738b9fd9ac6116339a85f015fb8f
Manik Yadav <manikyadav07@gmail.com>
2025-09-03 20:53:23 +0530

    Authcontext file correct path added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-import { AuthContextProvider } from './context/authContext.jsx';
+import { AuthContextProvider } from './context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
