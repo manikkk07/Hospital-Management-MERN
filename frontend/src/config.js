b15735d0b4b078b0147964dddbdf0d04326eac18
Manik Yadav <manikyadav07@gmail.com>
2025-09-04 14:16:59 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://hospital-management-mern-backend-x0e0.onrender.com";
+export const BASE_URL = "https://hospital-management-mern-backend-x0e0.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
